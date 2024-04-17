17177f7c32df40374c051c1159951e7945729517
dbtools-antcampo <antonio.campos@oracle.com>
2024-04-17 17:56:00 -0600

    feat(templates): set templates base linter configuration
    Merge pull request templates-add-linting

--- templates/node-vanilla/.eslintrc.cjs
+++ templates/node-vanilla/.eslintrc.cjs
@@ -10,52 +10,14 @@ module.exports = {
     root: true,
     env: { 
         browser: true,
-        es2020: true
+        es2020: true,
+        worker: true,
+        node: true
     },
     extends: [
         'eslint:recommended',
+        'plugin:node/recommended'
     ],
-    rules: {
-        'semi': [
-            'warn',
-            'always'
-        ],
-        'quotes': [
-            'warn',
-            'single'
-        ],
-        'array-bracket-spacing': [
-            'warn',
-            'always',
-            {
-                singleValue: true,
-                objectsInArrays: true,
-                arraysInArrays: true
-            }
-        ],
-        'space-in-parens': [
-            'warn',
-            'always'
-        ],
-        'computed-property-spacing': [
-            'warn',
-            'always'
-        ],
-        'object-curly-spacing': [
-            'warn',
-            'always'
-        ],
-
-        'no-console': [
-            'error',
-            {
-                allow: [
-                    'error',
-                    'warn',
-                ]
-            }
-        ],
-    },
     ignorePatterns: [
         'dist/**/*',
         'node_modules/**/*',
